bdbebd20b3aaed1f58f3e18527e70cb019bae828
tranthiminhthuytgdd15102003 <tranthiminhthuytgdd15102003@gmail.com>
2021-10-18 13:46:26 +0700

    add new demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int a,b;
+    float tong,hieu,tich,thuong;
+    cout << " Nhap a = ";
+    cin>>a;
+    cout << " Nhap b = "; 
+    cin>>b;
+    tong = a + b;
+    hieu = a - b;
+    tich = a * b;
+    thuong = a / b;
+    cout << "tong = " << tong << endl;
+    cout << "hieu = " << hieu << endl;
+    cout << "tich = " << tich << endl;
+    cout << "thuong = " << thuong << endl;
+    return 0;
+}
+
+    
